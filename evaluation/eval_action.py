a3f2fe5217318a3eaa92d3a4ab9709ccc8bbb5e9
daivd <daviddl997004866@gmail.com>
2019-02-24 23:19:22 +0800

    a little bugs 2 fix

--- evaluation/eval_action.py
+++ evaluation/eval_action.py
@@ -1,7 +1,8 @@
-from collections import defaultdict
-import numpy as np
-import json
 import argparse
+import json
+import sys
+
+import numpy as np
 
 
 def viou(traj_1, duration_1, traj_2, duration_2):
@@ -80,8 +81,8 @@ def voc_ap(rec, prec, use_07_metric=False):
 
 
 def eval_detection_scores(gt_actions, pred_actions, viou_threshold):
-    print(type(gt_actions), gt_actions)
-    print(type(pred_actions), pred_actions)
+    # print(type(gt_actions), gt_actions)
+    # print(type(pred_actions), pred_actions)
 
     pred_actions = sorted(pred_actions, key=lambda x: x['score'], reverse=True)
     gt_detected = np.zeros((len(gt_actions),), dtype=bool)
@@ -91,14 +92,10 @@ def eval_detection_scores(gt_actions, pred_actions, viou_threshold):
         k_max = -1
         for gt_idx, gt_action in enumerate(gt_actions):
             if not gt_detected[gt_idx] \
-                    and pred_action['category'] == gt_action['predicate']:
-                # s_iou = viou(pred_action['sub_traj'], pred_action['duration'],
-                #              gt_action['sub_traj'], gt_action['duration'])
-                # o_iou = viou(pred_action['obj_traj'], pred_action['duration'],
-                #              gt_action['obj_traj'], gt_action['duration'])
-                # ov = min(s_iou, o_iou)
+                    and pred_action['category'] == gt_action['category']:
+                # print(gt_action.keys())
                 ov = viou(pred_action['trajectory'], pred_action['duration'],
-                          gt_action, gt_action)
+                          gt_action['trajectory'], gt_action['duration'])
                 if ov >= viou_threshold and ov > ov_max:
                     ov_max = ov
                     k_max = gt_idx
@@ -111,13 +108,13 @@ def eval_detection_scores(gt_actions, pred_actions, viou_threshold):
     cum_fp = np.cumsum(fp).astype(np.float32)
     rec = cum_tp / np.maximum(len(gt_actions), np.finfo(np.float32).eps)
     prec = cum_tp / np.maximum(cum_tp + cum_fp, np.finfo(np.float32).eps)
+    print(prec, rec, hit_scores)
     return prec, rec, hit_scores
 
 
 def eval_tagging_scores(gt_actions, pred_actions):
     pred_actions = sorted(pred_actions, key=lambda x: x['score'], reverse=True)
     # ignore trajectories
-    # gt_triplets = set(tuple(r['triplet']) for r in gt_actions)
     gt_triplets = set(r['category'] for r in gt_actions)
     pred_triplets = []
     hit_scores = []
@@ -137,6 +134,7 @@ def eval_tagging_scores(gt_actions, pred_actions):
     cum_fp = np.cumsum(fp).astype(np.float32)
     rec = cum_tp / np.maximum(len(gt_triplets), np.finfo(np.float32).eps)
     prec = cum_tp / np.maximum(cum_tp + cum_fp, np.finfo(np.float32).eps)
+    print(prec, rec, hit_scores)
     return prec, rec, hit_scores
 
 
@@ -146,56 +144,107 @@ def eval_visual_action(groundtruth, prediction, viou_threshold=0.5,
     action tagging.
     """
     print('evaluating...')
-    video_ap = dict()
-    tot_scores = defaultdict(list)
-    tot_tp = defaultdict(list)
-    prec_at_n = defaultdict(list)
-    tot_gt_actions = 0
-    for vid, gt_actions in groundtruth.items():
-        # predict_actions = prediction[vid]
-        # print(vid, predict_actions)
-        if vid == 'version':
-            continue
-        predict_actions = prediction['results'][gt_actions]
-        gt_actions = groundtruth['relation_instances']
-        det_prec, det_rec, det_scores = eval_detection_scores(
-            gt_actions, predict_actions, viou_threshold)
-        tag_prec, _, _ = eval_tagging_scores(gt_actions, predict_actions)
-        # record per video evaluation results
-        video_ap[vid] = voc_ap(det_rec, det_prec)
-        tp = np.isfinite(det_scores)
-        for nre in det_nreturns:
-            cut_off = min(nre, det_scores.size)
-            tot_scores[nre].append(det_scores[:cut_off])
-            tot_tp[nre].append(tp[:cut_off])
-        for nre in tag_nreturns:
-            cut_off = min(nre, tag_prec.size)
-            prec_at_n[nre].append(tag_prec[cut_off - 1])
-        tot_gt_actions += len(gt_actions)
-    # calculate mean ap for detection
-    mAP = np.mean(video_ap.values())
-    # calculate recall for detection
-    rec_at_n = dict()
-    for nre in det_nreturns:
-        scores = np.concatenate(tot_scores[nre])
-        tps = np.concatenate(tot_tp[nre])
-        sort_indices = np.argsort(scores)[::-1]
-        tps = tps[sort_indices]
-        cum_tp = np.cumsum(tps).astype(np.float32)
-        rec = cum_tp / np.maximum(tot_gt_actions, np.finfo(np.float32).eps)
-        rec_at_n[nre] = rec[-1]
-    # calculate mean precision for tagging
-    mprec_at_n = dict()
-    for nre in tag_nreturns:
-        mprec_at_n[nre] = np.mean(prec_at_n[nre])
-    # print scores
-    print('detection mAP (used in challenge): {}'.format(mAP))
-    print('detection recall@50: {}'.format(rec_at_n[50]))
-    print('detection recall@100: {}'.format(rec_at_n[100]))
-    print('tagging precision@1: {}'.format(mprec_at_n[1]))
-    print('tagging precision@5: {}'.format(mprec_at_n[5]))
-    print('tagging precision@10: {}'.format(mprec_at_n[10]))
-    return mAP, rec_at_n, mprec_at_n
+
+    gt_version = groundtruth['version']
+    pred_version = prediction['version']
+    if gt_version != pred_version:
+        print("The version of groundtruth({}) and prediction({}) are different!"
+              .format(gt_version, pred_version))
+        sys.exit(0)
+
+    gt_video_id = groundtruth['video_id']
+    pred_video_id = list(prediction['results'].keys())[0]
+    if gt_video_id != pred_video_id:
+        print("The video id of groundtruth({}) and prediction({}) are different!"
+              .format(gt_video_id, pred_video_id))
+        sys.exit(0)
+
+    # get groundtruth
+    gt_actions = []
+    for each_ins in groundtruth['relation_instances']:
+
+        begin_fid = each_ins['begin_fid']
+        end_fid = each_ins['end_fid']
+
+        each_ins_trajectory = []
+        # end_fid += 1
+        for each_traj in groundtruth['trajectories'][begin_fid:end_fid]:
+            for each_traj_obj in each_traj:
+                if each_traj_obj['tid'] == each_ins['subject_tid']:
+                    each_traj_frame = [
+                        each_traj_obj['bbox']['xmin'],
+                        each_traj_obj['bbox']['ymin'],
+                        each_traj_obj['bbox']['xmax'],
+                        each_traj_obj['bbox']['ymax']
+                    ]
+                    each_ins_trajectory.append(each_traj_frame)
+
+        each_ins_action = {
+            "category": each_ins['predicate'],
+            "duration": [begin_fid, end_fid],
+            "trajectory": each_ins_trajectory
+        }
+
+        gt_actions.append(each_ins_action)
+
+    # print(json.dumps({"a": actions_instances}))
+
+    pred_actions = prediction['results'][pred_video_id]
+
+    eval_tagging_scores(gt_actions, pred_actions)
+
+    eval_detection_scores(gt_actions, pred_actions, viou_threshold)
+
+    # video_ap = dict()
+    # tot_scores = defaultdict(list)
+    # tot_tp = defaultdict(list)
+    # prec_at_n = defaultdict(list)
+    # tot_gt_actions = 0
+    # for vid, gt_actions in groundtruth.items():
+    #     # predict_actions = prediction[vid]
+    #     # print(vid, predict_actions)
+    #     if vid == 'version':
+    #         continue
+    #     predict_actions = prediction['results'][gt_actions]
+    #     gt_actions = groundtruth['relation_instances']
+    #     det_prec, det_rec, det_scores = eval_detection_scores(
+    #         gt_actions, predict_actions, viou_threshold)
+    #     tag_prec, _, _ = eval_tagging_scores(gt_actions, predict_actions)
+    #     # record per video evaluation results
+    #     video_ap[vid] = voc_ap(det_rec, det_prec)
+    #     tp = np.isfinite(det_scores)
+    #     for nre in det_nreturns:
+    #         cut_off = min(nre, det_scores.size)
+    #         tot_scores[nre].append(det_scores[:cut_off])
+    #         tot_tp[nre].append(tp[:cut_off])
+    #     for nre in tag_nreturns:
+    #         cut_off = min(nre, tag_prec.size)
+    #         prec_at_n[nre].append(tag_prec[cut_off - 1])
+    #     tot_gt_actions += len(gt_actions)
+    # # calculate mean ap for detection
+    # mAP = np.mean(video_ap.values())
+    # # calculate recall for detection
+    # rec_at_n = dict()
+    # for nre in det_nreturns:
+    #     scores = np.concatenate(tot_scores[nre])
+    #     tps = np.concatenate(tot_tp[nre])
+    #     sort_indices = np.argsort(scores)[::-1]
+    #     tps = tps[sort_indices]
+    #     cum_tp = np.cumsum(tps).astype(np.float32)
+    #     rec = cum_tp / np.maximum(tot_gt_actions, np.finfo(np.float32).eps)
+    #     rec_at_n[nre] = rec[-1]
+    # # calculate mean precision for tagging
+    # mprec_at_n = dict()
+    # for nre in tag_nreturns:
+    #     mprec_at_n[nre] = np.mean(prec_at_n[nre])
+    # # print scores
+    # print('detection mAP (used in challenge): {}'.format(mAP))
+    # print('detection recall@50: {}'.format(rec_at_n[50]))
+    # print('detection recall@100: {}'.format(rec_at_n[100]))
+    # print('tagging precision@1: {}'.format(mprec_at_n[1]))
+    # print('tagging precision@5: {}'.format(mprec_at_n[5]))
+    # print('tagging precision@10: {}'.format(mprec_at_n[10]))
+    # return mAP, rec_at_n, mprec_at_n
 
 
 if __name__ == '__main__':
@@ -211,11 +260,9 @@ if __name__ == '__main__':
     with open(args.prediction_file, 'r') as fin:
         prediction_json = json.load(fin)
 
-    mAP, rec_at_n, mprec_at_n = eval_visual_action(groundtruth_json, prediction_json)
+    # with open('test/task2/3598080384.json', 'r') as gt_f:
+    #     groundtruth_json = json.load(gt_f)
+    # with open('test/task2/task2.json', 'r') as gt_f:
+    #     prediction_json = json.load(gt_f)
 
-    # with open('/home/daivd/PycharmProjects/VORD/validation/0001/3598080384.json', 'r') as gt_r:
-    #     gr_json = json.load(gt_r)
-    #     print(gr_json.keys())
-        # ['version', 'video_id', 'video_path',
-        # 'frame_count', 'fps', 'width', 'height',
-        # 'subject/objects', 'trajectories', 'relation_instances']
+    mAP, rec_at_n, mprec_at_n = eval_visual_action(groundtruth_json, prediction_json)

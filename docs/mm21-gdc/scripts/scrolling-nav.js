31280e4b719d165ea7081065b6a1605263da1b56
李易聪 <liyicong@liyicongdeMacBook-Pro.local>
2021-03-07 13:55:08 +0800

    inti mm21-gdc

---
+++ docs/mm21-gdc/scripts/scrolling-nav.js
@@ -0,0 +1,10 @@
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
